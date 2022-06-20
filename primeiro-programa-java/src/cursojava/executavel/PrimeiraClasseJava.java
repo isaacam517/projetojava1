a0fdfa76ee81f3cbd995e816876f44a1929d8ac7
Isaac Martiniano <isaacam517@gmail.com>
2022-06-20 08:55:46 -0300

    Criação do Projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é um método auto executável em java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu Primeiro Programa em Java");
+		System.out.println("Java é Orientado a Objetos");
+	}
+}
